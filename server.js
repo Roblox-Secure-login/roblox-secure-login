d3c57f8bc90072524c564bf3c514789f6cd2ada1
Roblox-Secure-login <ebinsky3@gmail.com>
2025-03-10 21:36:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,15 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-// Simple in-memory storage (use a database in production)
 let storedData = [];
 
-// Endpoint to store data
 app.post('/store-data', (req, res) => {
   const { data } = req.body;
   storedData.push(data);
   res.send('Data stored successfully');
 });
 
-// Endpoint to retrieve stored data (authentication required)
 app.get('/get-data', (req, res) => {
-  // Add authentication logic here
   res.json({ data: storedData });
 });
 
